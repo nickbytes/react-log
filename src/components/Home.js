b43ade02c947b732126d5548fdfc3572364d09d7
Nick Beattie <nbeattie@Beattie-MacBook-Pro.local>
2016-08-24 10:00:53 -0700

    Use bikeshed and hello-color to determine UI colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { StyleSheet, css } from 'aphrodite';
-import { Toolbar, NavItem, Space } from 'rebass';
+import { Toolbar, NavItem, Space, Section } from 'rebass';
 import hello from 'hello-color';
 import bikeshed from '@jxnblk/bikeshed';
 
@@ -22,7 +22,7 @@ class Home extends Component {
     return (
       <div className={css(styles.app)}>
 
-          <Toolbar>
+          <Toolbar color={helloColor.color} backgroundColor={helloColor.base}>
             <NavItem is={Link} to="/">01–Log</NavItem>
 
             <Space auto x={1} />
@@ -30,9 +30,9 @@ class Home extends Component {
             <NavItem is={Link} to="/about">About</NavItem>
           </Toolbar>
 
-          <p>{helloColor.contrast}</p>
-
-        { React.cloneElement(this.props.children, this.props) }
+          <Section>
+            { React.cloneElement(this.props.children, this.props) }
+          </Section>
       </div>
     )
   }
