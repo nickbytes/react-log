8a6f0e9b493762a4465e0ee18bded33162094d02
Nick Beattie <nbeattie@Beattie-MacBook-Pro.local>
2016-08-24 13:25:16 -0700

    Messing aroung w rebass components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { Container, Heading, Section } from 'rebass';
+import { Container, Heading, SectionHeader } from 'rebass';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  narrow: {
+    maxWidth: '600px',
+    margin: '0 auto',
+    padding: '20px'
+  }
+})
 
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className={css(styles.narrow)}>
         <Container>
-          <Section>
-            <Heading>Purpose</Heading>
+          <div>
+            <SectionHeader>Purpose</SectionHeader>
             <p>The purpose of this project is ⌾. Please contemplate that.</p>
-          </Section>
+          </div>
+
+          <div>
+            <SectionHeader>A bit about your data.</SectionHeader>
+            <p>I don't have access to any of your data, it is stored on your computer using the help of your web browser. I don't want all that responsibility.</p>
+          </div>
         </Container>
       </div>
     )
