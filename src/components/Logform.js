72989482c3e6ed48d9ca42261641a281cb1a0811
Nick Beattie <nbeattie@Beattie-MBP.attlocal.net>
2016-08-26 08:20:17 -0400

    Form styling and attributes

--- src/components/Logform.js
+++ src/components/Logform.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import moment from 'moment';
+import { helloColor } from './Home';
 
 const styles = StyleSheet.create({
   space_vertical: {
@@ -9,6 +10,45 @@ const styles = StyleSheet.create({
   space_horizontal: {
     paddingLeft: '20px',
     paddingRight: '20px'
+  },
+  input: {
+    color: helloColor.base,
+    fontFamily: 'Fira Mono',
+    width: '100%',
+    padding: '10px 20px',
+    marginTop: '10px',
+    marginBottom: '20px',
+    border: '0',
+    fontSize: '3em',
+    lineHeight: '1.5em',
+    '-webkit-box-sizing': 'border-box',
+    '-moz-box-sizing': 'border-box',
+    'box-sizing': 'border-box',
+    '::-webkit-input-placeholder': {
+        fontFamily: 'Fira Mono',
+        color: helloColor.color,
+        opacity: '0.3'
+    },
+    ':-moz-placeholder': { /* Firefox 18- */
+       fontFamily: 'Fira Mono',
+       color: helloColor.color,
+       opacity: '0.3'
+    },
+
+    '::-moz-placeholder': {  /* Firefox 19+ */
+       fontFamily: 'Fira Mono',
+       color: helloColor.color,
+       opacity: '0.3'
+    },
+
+    ':-ms-input-placeholder': {
+       cfontFamily: 'Fira Mono',
+       color: helloColor.color,
+       opacity: '0.3'
+    }
+  },
+  label: {
+    Bottom: '10px'
   }
 });
 
@@ -30,7 +70,8 @@ class Logform extends Component {
     return (
       <div className={css(styles.space_vertical, styles.space_horizontal)}>
         <form ref="commentForm" onSubmit={this.handleSubmit}>
-          <input type="text" ref="myRefString" placeholder="Fixed it."/>
+          <label className={css(styles.label)} htmlFor="myRefString">Type your event and press Enter ⏎.</label>
+          <input className={css(styles.input)} type="text" id="myRefString" ref="myRefString" placeholder="Added Feature X to Fun Project Y."/>
           <input type="submit" hidden />
         </form>
       </div>
